1cdf0dbf56a962bc1d97fbe1a7b6426188972abf
Simon <simonstasko555@gmail.com>
2026-01-30 16:02:59 +0100

    Render only chat bubble

--- chatkit-bubble-react/src/App.tsx
+++ chatkit-bubble-react/src/App.tsx
@@ -1,12 +1,5 @@
 import { ChatBubble } from "./ChatBubble";
 
 export default function App() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Moja stránka</h1>
-      <p>Vpravo dole je ChatKit bublina.</p>
-
-      <ChatBubble />
-    </div>
-  );
+  return <ChatBubble />;
 }
